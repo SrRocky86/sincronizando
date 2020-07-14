30f23a4030c4e8730f55dd76765a385ddbf43d0e
SrRocky86 <68125896+SrRocky86@users.noreply.github.com>
2020-07-14 10:28:31 -0700

    Update nav.php

--- nav.php
+++ nav.php
@@ -1,9 +1,9 @@
 
 <header>
   <ol>
-    <li><a href="m">Home</a></li>
-    <li><a href="m">Quienes somos</a></li>
-    <li><a href="m">Nosotros</a></li>
-    <li><a href="m">Nosotros</a></li>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Quienes somos</a></li>
+    <li><a href="#">Nosotros</a></li>
+    <li><a href="#">Nosotros</a></li>
   </ol>
 </header>
